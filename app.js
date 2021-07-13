e23769136b56f5043aa34b0eeeeb79bdb903a9c7
shohruh-xasanov <pfxasanov.shoxrux@gmail.com>
2021-07-14 00:37:27 +0500

    push

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+const express=require('express')
+const ejs=require('ejs')
+const path=require('path')
+const PORT= 5000
+const bodyParser=require('body-parser')
+const layouts=require('express-ejs-layouts')
+const app=express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(layouts)
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.render('page/index',{
+        layout:'./page/layout'
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log("localhostda ishlayapdi")
+})
